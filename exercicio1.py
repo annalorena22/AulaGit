3d66df4e8f5f3f57b63434a6ff21f8af73c8d948
annalorena22 <annalorena1998@gmail.com>
2024-10-16 10:56:41 -0400

    exercício 1 finalizado

--- exercicio1.py
+++ exercicio1.py
@@ -1,1 +1,14 @@
 #Criar uma função que calcule o fatorial de um numero dado pelo usuário.
+
+numero = int(input("Digite um número para calcular o fatorial: "))
+
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado *= i
+        return resultado
+
+print(f"O fatorial de {numero} é {fatorial(numero)}.")
